b78dafeb9a9482639593e3a5d03a437369a36699
Aiden Bai <aiden.bai05@gmail.com>
2022-06-01 09:04:28 -0700

    React config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { million } from 'million/vite-plugin-million';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-const reactCompatPath = resolve(__dirname, './node_modules/million/dist/react.mjs');
-
 export default defineConfig({
-  plugins: [million()],
-  resolve: {
-    alias: {
-      react: reactCompatPath,
-      'react-dom': reactCompatPath,
-    },
-  },
+  plugins: [million({ react: true })],
 });
