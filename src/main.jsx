8451125cc0dff4341bee1d0089fbd3b905802028
Aiden Bai <aiden.bai05@gmail.com>
2022-06-14 18:24:27 -0700

    fix: add requestIdleCallback polyfill

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'requestidlecallback-polyfill';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
